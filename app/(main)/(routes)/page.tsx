5adf1ccb81ef8f5076fcf59565d6c2a142647492
Subhranshu Pati <subhranshupati0412@gmail.com>
2023-08-26 22:25:07 +0530

    Login and SIgn Up

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <p>This is Protected Route</p>
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
   )
 }
