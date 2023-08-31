7d9b7ff8d28f918ac49ebbe44d1477666a855db6
Subhranshu Pati <subhranshupati0412@gmail.com>
2023-08-31 11:30:50 +0530

    Initial Modal

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,4 +1,8 @@
 "use client";
+import { useForm } from "react-hook-form";
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
 import {
     Dialog,
     DialogContent,
@@ -7,9 +11,36 @@ import {
     DialogHeader,
     DialogTitle
 } from "@/components/ui/dialog";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { useEffect, useState } from "react";
+
+const formSchema = z.object({
+    name: z.string().min(1, { message: "Server name is required" }),
+    imageUrl: z.string().min(1, { message: "Server image is required" })
+})
 
 
 const InitialModal = () => {
+    const [isMounted, setIsMounted] = useState(false);
+    useEffect(() => {
+        setIsMounted(true)
+    }, [])
+
+
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            imageUrl: ""
+        }
+    });
+    const isLoading = form.formState.isSubmitting;
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        console.log(values)
+    }
+    if (!isMounted) return null;
     return (
         <Dialog open>
             <DialogContent className="p-0 overflow-hidden text-black bg-white">
@@ -21,7 +52,40 @@ const InitialModal = () => {
                         You can always change these settings later.
                     </DialogDescription>
                 </DialogHeader>
-
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        <div className="px-6 space-y-8">
+                            <div className="flex items-center justify-center text-center">
+                                !Image Upload
+                            </div>
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel className="text-xs font-bold uppercase text-zinc-500 dark:text-secondary/70">
+                                            Server Name
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                disabled={isLoading}
+                                                className="text-black border-0 bg-zinc-300/50 focus-visible:ring-0 focus-visible:ring-offset-0"
+                                                placeholder="Enter a server name"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <DialogFooter className="px-6 py-4 bg-gray-100">
+                            <Button disabled={isLoading} variant="primary">
+                                Create
+                            </Button>
+                        </DialogFooter>
+                    </form>
+                </Form>
             </DialogContent>
         </Dialog>
     );
